151179cbf2e67f941617c1af25baef62021c57cd
MRDGH2821 <mihirrabade@gmail.com>
2022-05-16 09:49:47 +0530

    chore: extract prewitt into seperate variable

--- Edge_Segmentation.py
+++ Edge_Segmentation.py
@@ -27,6 +27,7 @@ kernelx = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
 kernely = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 img_prewittx = cv2.filter2D(img_gaussian, -1, kernelx)
 img_prewitty = cv2.filter2D(img_gaussian, -1, kernely)
+img_prewitt = img_prewittx + img_prewitty
 
 # laplacian
 img_laplace = cv2.convertScaleAbs(
@@ -38,7 +39,7 @@ cv2.imshow("Sobel Y", img_sobely)
 cv2.imshow("Sobel", img_sobel)
 cv2.imshow("Prewitt X", img_prewittx)
 cv2.imshow("Prewitt Y", img_prewitty)
-cv2.imshow("Prewitt", img_prewittx + img_prewitty)
+cv2.imshow("Prewitt", img_prewitt)
 cv2.imshow("Laplacian of Gaussian", img_laplace)
 
 cv2.waitKey(0)
